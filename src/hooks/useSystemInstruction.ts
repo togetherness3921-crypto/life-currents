7e7d513ab96b8d35028de759c59d3a7f3394cf2d
Your Name <you@example.com>
2025-09-24 12:50:26 -0500

    Auto-sync: 9/24/2025, 12:50:26 PM

---
+++ src/hooks/useSystemInstruction.ts
@@ -0,0 +1,10 @@
+import { useContext } from 'react';
+import { SystemInstructionContext } from './systemInstructionProvider';
+
+export const useSystemInstruction = () => {
+  const ctx = useContext(SystemInstructionContext);
+  if (!ctx) {
+    throw new Error('useSystemInstruction must be used within a SystemInstructionProvider');
+  }
+  return ctx;
+};
