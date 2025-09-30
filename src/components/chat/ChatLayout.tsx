4bb89f0d4e144fe5959ebafe1704bcae96be477c
togetherness3921-crypto <togetherness3921@gmail.com>
2025-09-30 15:45:52 -0500

    feat: add chat context settings dialog

--- src/components/chat/ChatLayout.tsx
+++ src/components/chat/ChatLayout.tsx
@@ -4,6 +4,7 @@ import ChatSidebar from './ChatSidebar';
 import ChatPane from './ChatPane';
 import { ChatProvider } from '@/hooks/chatProvider';
 import { SystemInstructionsProvider } from '@/hooks/systemInstructionProvider';
+import { ContextSettingsProvider } from '@/hooks/contextSettingsProvider';
 import { ModelSelectionProvider } from '@/hooks/modelSelectionProvider';
 import { McpProvider } from '@/hooks/mcpProvider';
 
@@ -12,17 +13,19 @@ const ChatLayout = () => {
         <McpProvider>
             <ModelSelectionProvider>
                 <SystemInstructionsProvider>
-                    <ChatProvider>
-                        <ResizablePanelGroup direction="horizontal" className="h-full w-full">
-                            <ResizablePanel defaultSize={20} minSize={15} maxSize={30}>
-                                <ChatSidebar />
-                            </ResizablePanel>
-                            <ResizableHandle withHandle />
-                            <ResizablePanel>
-                                <ChatPane />
-                            </ResizablePanel>
-                        </ResizablePanelGroup>
-                    </ChatProvider>
+                    <ContextSettingsProvider>
+                        <ChatProvider>
+                            <ResizablePanelGroup direction="horizontal" className="h-full w-full">
+                                <ResizablePanel defaultSize={20} minSize={15} maxSize={30}>
+                                    <ChatSidebar />
+                                </ResizablePanel>
+                                <ResizableHandle withHandle />
+                                <ResizablePanel>
+                                    <ChatPane />
+                                </ResizablePanel>
+                            </ResizablePanelGroup>
+                        </ChatProvider>
+                    </ContextSettingsProvider>
                 </SystemInstructionsProvider>
             </ModelSelectionProvider>
         </McpProvider>
